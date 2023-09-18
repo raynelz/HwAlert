6ec35f59af530e572cdf19cdb0a83d419bf89362
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-09-18 19:22:51 +0300

    comlete 1 half of hw

--- HwAlert/ViewController.swift
+++ HwAlert/ViewController.swift
@@ -3,7 +3,7 @@
 // 1.Написать свое мини приложение на первом экране две кнопки "сложение" и "угадай число" ✅
 // 2. При попадании на этот экран запрашивать ФИО через алер и отображать в UILabel ✅
 // 3. По нажатию на "сложение" открывать Алерт который просит ввести число 1 и число 2 и кнопка посчитать выводит результат в любом виде юзеру ✅
-// 4. При нажатии на кнопку "угадай число" нужно придумать игру где нужно угадать число
+// 4. При нажатии на кнопку "угадай число" нужно придумать игру где нужно угадать число ✅
 // 5. Создать второе приложение MVC у которого на экране будет кнопка "начать" которая вызывает Алерт с сообщением "введите слово"
 // 6. Нужно ввести "leohl" в Алерт нажимаете "ок" отдаете контролеру а контролер отдаст это в модель которая из буквосочетания "leohl" вернет слово "hello" контролеру а контролер отдаст это лейблу и отобразит это на экране
 // 7. Все кодом пишем!
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
     // MARK: LABELS
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var sumLabel: UILabel!
-
+    @IBOutlet weak var guessLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -30,7 +31,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func guessNumberButton(_ sender: Any) {
-
+        guessNumberAlert(title: "Угадай случайно загаданое число", message: "Диапазон от 0 до 10", style: .alert)
     }
 
     // MARK: ALERTS
@@ -71,9 +72,34 @@ class ViewController: UIViewController {
 
     // guess the number
     func guessNumberAlert(title: String, message: String, style: UIAlertController.Style) {
+        
+        // Variables
+        let randomNum = Int.random(in: 0...10)
+        var correctAnswer = false
+        
+        // Controller
         let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        let action = UIAlertAction(title: "Проверить", style: .default) { _ in }
+        let action = UIAlertAction(title: "Проверить", style: .default) { _ in
+            let userNum = alertController.textFields?.first
+            
+            // Check the user answer with random number
+            if (userNum?.text == String(randomNum)) {
+                correctAnswer = true
+            } else {
+                correctAnswer = false
+            }
+            
+            self.guessLabel.text! = "Ответ: " + String(correctAnswer)
+        }
+        
+        alertController.addTextField { _ in
+            
+        }
+        
+        // Add actions
         alertController.addAction(action)
+        
+        // Add to view hierarchy
         present(alertController, animated: true)
     }
 }
